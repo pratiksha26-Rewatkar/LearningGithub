103528b198967388b5e89e3c4f97531f89401345
pratiksha26-Rewatkar <106666938+pratiksha26-Rewatkar@users.noreply.github.com>
2024-12-14 22:08:57 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,11 @@
+package com.spring.orm.entities;
+
+import org.hibernate.persistence.Entity;
+
+@Entity
+public class Student {
+      private int studentId;
+      private String studentName;
+      private String studentCity;
+      
+}
